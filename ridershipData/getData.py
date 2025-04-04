d68ee87b2548c9d01ff6a09ef0096469b585f0d8
Aron Connors <aronconnorsdev@gmail.com>
2025-04-04 17:09:32 -0400

    first dataset

---
+++ ridershipData/getData.py
@@ -0,0 +1,45 @@
+import requests
+import csv
+import time
+
+base_url = "https://data.ny.gov/resource/jsu2-fbtj.json"
+SQL = """
+SELECT *
+WHERE origin_station_complex_id = 610 and destination_station_complex_id = 628
+ORDER BY
+timestamp
+"""
+limit = 1000
+offset = 0
+all_data = []
+
+#break the query down into multiple requests to avoid the default limit
+while True:
+    paginated_query = f"{SQL} LIMIT {limit} OFFSET {offset}"
+    params = {
+        "$query": paginated_query
+    }
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    #if the data is not in list form, there is something wrong with it. Otherwise its done
+    if not isinstance(data, list):
+        print(data)
+        break
+    elif not data:
+        print("Done")
+        break
+
+
+    all_data.extend(data)
+    #increase the offset by the limit size so we can get the next piece of the response sequentially
+    offset += limit
+
+    time.sleep(0.2)
+
+#save output to csv file and save seperately when necessary
+with open("ridership_output.csv", "w", newline='', encoding='utf-8') as csvfile:
+    if all_data:
+        writer = csv.DictWriter(csvfile, fieldnames=all_data[0].keys())
+        writer.writeheader()
+        writer.writerows(all_data)
