f76c011356e2790ea3f6f2eb0fef93220bdaf929
Aron Connors <aronconnorsdev@gmail.com>
2025-04-07 02:48:28 -0400

    crimeDataPdfExtraction

--- ridershipData/getData.py
+++ ridershipData/getData.py
@@ -4,10 +4,8 @@ import time
 
 base_url = "https://data.ny.gov/resource/jsu2-fbtj.json"
 SQL = """
-SELECT *
-WHERE origin_station_complex_id = 610 and destination_station_complex_id = 628
-ORDER BY
-timestamp
+SELECT origin_station_complex_name, destination_station_complex_name, max(estimated_average_ridership)
+GROUP BY origin_station_complex_name, destination_station_complex_name
 """
 limit = 1000
 offset = 0
