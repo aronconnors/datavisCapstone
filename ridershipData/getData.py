b7e045279353e3d6dabccb06d98eabd4f2261b63
Aron Connors <aronconnorsdev@gmail.com>
2025-04-12 10:15:30 -0400

    chef aron and chef quattro

--- ridershipData/getData.py
+++ ridershipData/getData.py
@@ -4,39 +4,59 @@ import time
 
 base_url = "https://data.ny.gov/resource/jsu2-fbtj.json"
 SQL = """
-select count(*)
+SELECT
+    month,
+    day_of_week,
+    hour_of_day,
+    destination_station_complex_name,
+    sum(estimated_average_ridership)
+GROUP BY
+    destination_station_complex_name,
+    month,
+    day_of_week,
+    hour_of_day
 """
-limit = 1000
+
+file = "arrivals_output.csv"
+
+limit = 999
 offset = 0
 all_data = []
+with open(file, "w", newline='', encoding='utf-8') as csvfile:
+    #break the query down into multiple requests to avoid the default limit
+    header = True
+    while True:
+        paginated_query = f"{SQL} LIMIT {limit} OFFSET {offset}"
+        params = {
+            "$query": paginated_query
+        }
+        if header:
+            print('getting...')
+        else:
+            print('getting more...')
 
-#break the query down into multiple requests to avoid the default limit
-while True:
-    paginated_query = f"{SQL} LIMIT {limit} OFFSET {offset}"
-    params = {
-        "$query": paginated_query
-    }
-    response = requests.get(base_url, params=params)
-    data = response.json()
+        response = requests.get(base_url, params=params)
+        data = response.json()
+        print('got')
 
-    #if the data is not in list form, there is something wrong with it. Otherwise its done
-    if not isinstance(data, list):
-        print(data)
-        break
-    elif not data:
-        print("Done")
-        break
+        #if the data is not in list form, there is something wrong with it. Otherwise its done
+        if not isinstance(data, list):
+            print(data)
+            break
+        elif not data:
+            print("Done")
+            break
+
+        #increase the offset by the limit size so we can get the next piece of the response sequentially
+        offset += len(data)
+
+        time.sleep(60)
 
 
-    all_data.extend(data)
-    #increase the offset by the limit size so we can get the next piece of the response sequentially
-    offset += limit
-
-    time.sleep(0.2)
-
-#save output to csv file and save seperately when necessary
-with open("ridership_output.csv", "w", newline='', encoding='utf-8') as csvfile:
-    if all_data:
-        writer = csv.DictWriter(csvfile, fieldnames=all_data[0].keys())
-        writer.writeheader()
-        writer.writerows(all_data)
+    #save output to csv file and save seperately when necessary
+        
+        writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
+        if header:
+            writer.writeheader()
+        writer.writerows(data)
+        header = False
